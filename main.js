d93cece320c4a3d8ba7f7e89c0182b3d77e6815d
Kumara Undru <kundru@fanatics.com>
2017-11-14 15:46:44 -0800

    Remove specs;

--- main.js
+++
@@ -1,9 +0,0 @@
-const Launcher = require('./build/launcher').Launcher
-
-new Launcher({
-  specs: ['specs/*.test.js'],
-  capabilities: [{
-    browserName: 'chrome',
-    maxInstances: 2
-  }]
-}).run()
