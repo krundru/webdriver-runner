f3b82ecc3b38147dc1ea53f21d64e9d7e7598b4f
Kumara Undru <kundru@fanatics.com>
2018-02-26 07:25:32 -0800

    Add examples

---
+++ examples/runner.js
@@ -0,0 +1,27 @@
+const Launcher = require('webdriver-runner/lib/launcher').Launcher
+const SpecSummary = require('webdriver-runner/lib/reporters/spec-summary')
+
+const request = {
+  mochaOptions: {
+    retries: 1,
+    requires: ['babel-core/register'],
+    timeout: 900000
+  },
+  // These flags will be available throw process.argv on test scripts.
+  execArgv: [
+    '--upload',
+    '--env=prod'
+  ],
+  reporters: [SpecSummary],
+  reporterOptions: {},
+  specs: [{
+    tests: ['*.test.js'],
+    browsers: [{
+      name: 'chrome',
+      // No of parallel runs.
+      shard: 3
+    }]
+  }]
+}
+
+new Launcher(request).run()
