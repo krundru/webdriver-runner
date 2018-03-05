763a30cfd6f14b1ba7df27cfa9cec49783204aaf
krundru <24993489+krundru@users.noreply.github.com>
2018-03-05 08:05:55 -0800

    Add delegate method to get actual element (#7)

--- examples/homepage.test.js
+++ examples/homepage.test.js
@@ -4,7 +4,7 @@ import webdriverProvider from './webdriver-provider'
 import test from 'webdriver-runner/testing'
 import wdUtil from 'webdriver-runner/util'
 import * as syncr from 'webdriver-runner/synchronize'
-import { By } from 'selenium-webdriver'
+import { By, until } from 'selenium-webdriver'
 
 test.describe('Github home page', function() {
   let driver
@@ -19,6 +19,12 @@ test.describe('Github home page', function() {
     const link = driver.findElements(By.css('.HeaderNavlink'))[4]
     // Navigate to pricing page
     link.click()
+
+    // Use delegate() method to return actual WebElement, instead of wrapper
+    // This method is useful incase of 'Until.*' methods
+    const ele = driver.findElement(By.css('.plans-card'))
+    driver.wait(until.elementIsVisible(ele.delegate()))
+
     // Query available plans
     const plans = driver.findElements(By.css('.plans-card'))
     console.log('Number of plans available: ', plans.length)
